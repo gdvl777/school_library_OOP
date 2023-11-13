c50a921be6d6c10533f4db4bcb3670f409f266e7
ITurres <emanueliturres.co@gmail.com>
2023-11-13 17:46:13 -0300

    Format: Fix indentation, naming-convention and more on 'user_interface.rb' module

--- modules/user_interface.rb
+++ modules/user_interface.rb
@@ -1,5 +1,5 @@
-module user_interface
-    MENU_OPTIONS = {
+module UserInterface
+  MENU_OPTIONS = {
     1 => :list_books,
     2 => :list_people,
     3 => :create_person,
@@ -7,21 +7,21 @@ module user_interface
     5 => :create_rental,
     6 => :list_rentals_for_person,
     7 => :exit_app
-    }.freeze
+  }.freeze
 
-    def display_menu
+  def display_menu
     puts "\nPlease choose an option:"
     MENU_OPTIONS.each { |key, value| puts "#{key} - #{value.to_s.tr('_', ' ')}" }
     print 'Option: '
     gets.chomp.to_i
-    end
+  end
 
-    def handle_option(app, option)
+  def handle_option(app, option)
     action = MENU_OPTIONS[option]
-        if action
-            app.send(action)
-        else
-            puts 'Invalid option. Please try again.'
-        end
+    if action
+      app.send(action)
+    else
+      puts 'Invalid option. Please try again.'
     end
+  end
 end
