188df0520ae26b21ff774c75a14ec4f652016395
ITurres <emanueliturres.co@gmail.com>
2023-11-13 18:44:33 -0300

    Fix: 'create_entities.rb' relative files path

--- modules/create_entities.rb
+++ modules/create_entities.rb
@@ -1,9 +1,9 @@
-require_relative 'classroom'
-require_relative 'person'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'book'
-require_relative 'rental'
+require_relative '../classroom'
+require_relative '../person'
+require_relative '../student'
+require_relative '../teacher'
+require_relative '../book'
+require_relative '../rental'
 
 module CreateEntities
   def create_person
