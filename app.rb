f592a0a95bbce4bdde3149a460eaa6fed772ec62
ITurres <emanueliturres.co@gmail.com>
2023-11-14 20:17:58 -0300

    Format: Fix double line separators format issue
    - On 'app.rb' and 'modules/rentals_data_manipulation.rb'

--- app.rb
+++ app.rb
@@ -1,65 +1,39 @@
 require 'json'
-
 require 'fileutils'
-
 require_relative 'book'
-
 require_relative 'person'
-
 require_relative 'student'
-
 require_relative 'teacher'
-
 require_relative 'rental'
-
 require_relative 'classroom'
 
-
-
 require_relative 'modules/create_entities'
-
 require_relative 'modules/list_entities'
-
 require_relative 'modules/rentals_data_manipulation'
-
 require_relative 'modules/people_data_manipulation'
 
-
-
 RENTALS_PATH = './db/rentals.json'.freeze
-
 PEOPLE_PATH = './db/people.json'.freeze
-
-
 
 class App
   include CreateEntities
-
   include ListEntities
-
   include RentalsDataManipulation
-
   include PeopleDataManipulation
 
   attr_accessor :books, :people, :rentals
-
-
 
   def initialize
     FileUtils.mkdir_p('./data')
 
     @books = []
-
     @people = load_people_from_json(PEOPLE_PATH)
-
     @rentals = load_rentals_from_json(RENTALS_PATH)
   end
 
   def create_person
     # * First it will promp the user to choose between student or teacher
-
     person = super # ! super = create_person from CreateEntities module.
-
     # * Then it will return the person instance.
 
     @people << person
@@ -69,9 +43,7 @@ class App
 
   def create_book
     # * First it will promp the user for the creation of a book.
-
     book = super # ! super = create_book from CreateEntities module.
-
     # * Then it will return the book instance.
 
     @books << book
@@ -79,20 +51,12 @@ class App
 
   def create_rental
     # * First it will promp the user for the creation of a rental, by
-
     # * iterating over the books and people arrays, and asking the user
-
     # * to choose one of each.
-
     rental = super(@books, @people) # ! super = create_rental from CreateEntities module.
-
     # * Then it will return the rental instance.
 
-
-
     @rentals << rental # * adds the rental to the rentals array which already has the rentals loaded from the json file.
-
-
 
     save_rentals_to_json(RENTALS_PATH, rental)
   end
@@ -103,20 +67,13 @@ class App
     exit
   end
 
-
-
-  # Nuevas funciones agregadas para manejar JSON
-
   def save_to_files
     File.write('./data/books.json', JSON.dump(@books.map(&:to_h)))
-
     File.write('./data/people.json', JSON.dump(@people.map(&:to_h)))
   end
 
   def load_from_files
     @books = load_data('./data/books.json') { |book| Book.new(book['title'], book['author']) }
-
-
 
     @people = load_data('./data/people.json') do |person|
       new_person = Person.new(person['name'], person['age'])
@@ -129,8 +86,6 @@ class App
 
   def load_data(file_name, &block)
     return [] unless File.exist?(file_name)
-
-
 
     json_data = JSON.parse(File.read(file_name))
 
